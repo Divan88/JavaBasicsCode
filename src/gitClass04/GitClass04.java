e75d494436aa56b06040bda908b81faabff074a1
Ivan <dugave.ivan@gmail.com>
2022-06-01 21:20:47 -0400

    changes in GitClass04

---
+++ src/gitClass04/GitClass04.java
@@ -0,0 +1,12 @@
+package gitClass04;
+
+public class GitClass04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	System.out.println("popculture");
+	}
+
+}
